38bbb60dc61d981cbf6d7aa529a68d9a2ccd2507
ADITYA PRASAD <adityajvm22@gmail.com>
2025-07-03 11:03:47 +0530

    registerUser defined, user route initiated and deletelocal files on signup of existed user

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,61 @@
+import { asyncHandler } from "../utils/AsyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+import { deleteLocalFiles } from "../utils/deleteLocalFiles.js";
+
+
+const registerUser=asyncHandler(async(req,res)=>{
+    //getting data
+    const {username,email,fullName,password}=req.body;
+    //checking for valid data
+    if([username,email,fullName,password].some((field)=>field?.trim==="")){
+        throw new ApiError(400,"All fields are required")
+    }
+    // getting avatar from files
+    const avatarLocalPath=req.files?.avatar[0]?.path;
+    if(!avatarLocalPath){
+        throw new ApiError(400,"Avatar is required")
+    }
+
+    const existedUser=await User.findOne({
+        $or:[{username},{email}]
+    })
+    if(existedUser){
+        deleteLocalFiles([avatarLocalPath]) // neccessaey as if existed user re enters the images are saved locally though not uploded on cloud
+        throw new ApiError(400,"User already exists")
+    }
+
+    const avatar=await uploadOnCloudinary(avatarLocalPath);
+    if(!avatar){
+        throw new ApiError(400,"failed to upload on cloudinary")
+    }
+
+    const user=await User.create({
+        fullName,
+        avatar:avatar.url,
+        email,
+        password,
+        username:username.toLowerCase()
+
+    })
+
+    const createdUser=await User.findById(user._id).select("-password -refreshToken")
+    if(!createdUser){
+        throw new ApiError(500,"user not registered!! Server problem")
+    }
+
+    return res
+    .status(201)
+    .json(
+        new ApiResponse(201,createdUser,"User registered successfully!!!")
+    )
+
+
+})
+
+
+export {registerUser}
